2a5a76320d55d2d4d992eb0bd09846e80eaf4a8d
ThanhChau82 <thanh.dalencon@gmail.com>
2021-10-11 12:07:10 +0200

    Spring Boot Library project initialization for managing books.
    Package by feature architecture. Angular as client web, Spring Boot Java project (with Spring Data JPA) as back-end project and H2 Database.

---
+++ src/main/java/fr/projetAngularJPA/bibliotheque/BibliothequeAngularJpaApplication.java
@@ -0,0 +1,13 @@
+package fr.projetAngularJPA.bibliotheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliothequeAngularJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliothequeAngularJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/projetAngularJPA/bibliotheque/BibliothequeAngularJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.projetAngularJPA.bibliotheque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliothequeAngularJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
